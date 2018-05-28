487579a5a30b6763a8013a4660b7428a4961b5a6
orangain <orangain@gmail.com>
2018-05-28 22:33:42 +0900

    Now map result to DTO directly

--- src/main/java/com/example/resource/CustomerResource.java
+++ src/main/java/com/example/resource/CustomerResource.java
@@ -1,9 +1,6 @@
 package com.example.resource;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -12,7 +9,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
-import org.apache.commons.beanutils.BeanUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -30,19 +26,7 @@ public class CustomerResource {
 		ApiApplication application = (ApiApplication) ((ResourceConfig) this.application).getApplication();
 
 		try (SqlSession session = application.openSession()) {
-			List<Map<String, Object>> customers = session.selectList("com.example.customerSelect");
-
-			return customers.stream().map(entity -> {
-				CustomerDTO customerDTO = new CustomerDTO();
-				entity.forEach((key, value) -> {
-					try {
-						BeanUtils.copyProperty(customerDTO, key, value);
-					} catch (IllegalAccessException | InvocationTargetException e) {
-						throw new RuntimeException(e);
-					}
-				});
-				return customerDTO;
-			}).collect(Collectors.toList());
+			return session.selectList("com.example.customerSelect");
 		}
 	}
 }
